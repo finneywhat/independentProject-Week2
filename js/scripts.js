f5b22ef9ee8fe0ff2b6e71ffe40fe7c050002d8d
Chris Finney <the.finney@gmail.com>
2017-02-17 14:35:01 -0800

    save master branch prior to creation of new branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var totalVal = function(question1, question2, question3) {
+  return question1 + question2 + question3;
+}
+
 $(document).ready(function(){
   $("form#mainForm").submit(function(event) {
     event.preventDefault();
@@ -8,14 +12,14 @@ $(document).ready(function(){
   var question4 = $("#question4").val();
   var question5 = $("#question5").val();
   var question6 = $("#question6").val();
-  
+
   debugger;
 
-    if (question1 === "1") {
+    if (question1 === "1" || question2 === "1") {
         $("#suggestedTracks, #java").show();
         $("#c-sharp").hide();
         $("#php").hide();
-      } else if (question1 === "2") {
+      } else if (question1 === "2" || ) {
         $("#suggestedTracks, #c-sharp").show();
         $("#php").hide();
         $("#java").hide();
@@ -24,6 +28,8 @@ $(document).ready(function(){
         $("#c-sharp").hide();
         $("#java").hide();
       }
+
+
      $("form#mainForm").change();
 
   });
