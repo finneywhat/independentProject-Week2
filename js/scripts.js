b7a8a7883548f2805f94d7a001370270c6f222cd
Chris Finney <the.finney@gmail.com>
2017-02-17 12:53:33 -0800

    modifies index.html by commenting out all but one survey question and starting with one question in one form. jQuery now works to display selected track.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-  $("form#mainForm").submit(function(event){
-    debugger;
-    // $(".btn").click(function() {
-      $("#suggestedTracks, #java").show();
-    // });
-    event.preventDefault();
+  $("form#mainForm").submit(function(event) {
+
+  var question1 = $("input:radio[name=question1]:checked").val();
+  debugger;
+
+    if (question1 === "1") {
+        $("#suggestedTracks, #java").show();
+      } else if (question1 === "2") {
+        $("#suggestedTracks, #c-sharp").show();
+      } else {
+        $("#suggestedTracks, #php").show();
+     }
+
+     $("form#mainForm").change();
+
+     event.preventDefault();
   });
 });
