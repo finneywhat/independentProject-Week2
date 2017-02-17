b0e8bcf094ce7d7d1cf7b6b060e0e2c02df61b97
Chris Finney <the.finney@gmail.com>
2017-02-17 14:56:13 -0800

    add bulk of javascript functionality to add input values and determine suggestion based on branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
-var totalVal = function(question1, question2, question3) {
-  return question1 + question2 + question3;
-}
+var totalVal = function(number1, number2, number3) {
+  return number1 + number2 + number3;
+};
 
 $(document).ready(function(){
   $("form#mainForm").submit(function(event) {
     event.preventDefault();
 
-  var question1 = $("#question1").val();
-  var question2 = $("#question2").val();
-  var question3 = $("#question3").val();
-  var question4 = $("#question4").val();
-  var question5 = $("#question5").val();
-  var question6 = $("#question6").val();
-
+  var question1 = parseInt($("#question1").val());
+  var question2 = parseInt($("#question2").val());
+  var question3 = parseInt($("#question3").val());
+  var question4 = parseInt($("#question4").val());
+  var question5 = parseInt($("#question5").val());
+  var question6 = parseInt($("#question6").val());
+  var result = totalVal(question1, question2, question3);
   debugger;
 
-    if (question1 === "1" || question2 === "1") {
+    if (result <= 4) {
         $("#suggestedTracks, #java").show();
         $("#c-sharp").hide();
         $("#php").hide();
-      } else if (question1 === "2" || ) {
+      } else if (result <= 7) {
         $("#suggestedTracks, #c-sharp").show();
         $("#php").hide();
         $("#java").hide();
